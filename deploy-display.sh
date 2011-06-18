87018f77b43c5d1778a277bca3b480fdfba7ede2
Artem Baguinski <artm@v2.nl>
2011-06-18 13:43:59 +0200

    deploy uses url as known on the adhoc network

--- deploy-display.sh
+++ deploy-display.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avuz --delete ~/src/BioFame/BioDisplay-debug/BioDisplay.app marnixdenijs@bioscreen:src/BioFame
+rsync -avuz --delete ~/src/BioFame/BioDisplay-debug/BioDisplay.app bioscreen.local:src/BioFame
